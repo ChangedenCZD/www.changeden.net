aad95e6b32e4e2a898486aeb5053c4e29bdaf6fa
Changeden <1499450463@qq.com>
2017-09-08 16:59:10 +0800

    更新

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var lessMiddleware = require('less-middleware');
-// var index = require('./routes/index');
-// var users = require('./routes/users');
-var app = express();
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let lessMiddleware = require('less-middleware');
+// let index = require('./routes/index');
+// let users = require('./routes/users');
+let app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    /* res.header('Access-Control-Allow-Origin', '*');
+    let path = req.path;
+    if (path.startsWith('/api/')) {
+        res.header('Access-Control-Allow-Origin', 'https://changeden.net');
+        let origin = req.headers.origin || '';
+        if (origin.endsWith('.changeden.net') || origin.endsWith('//changeden.net')) {
+            res.header('Access-Control-Allow-Origin', origin);
+        }
+    } */
+    res.header('Access-Control-Allow-Origin', 'https://changeden.net');
+    next();
+    // Access-Control-Allow-Headers:Origin, X-Requested-With, Content-Type, Accept
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -30,7 +47,7 @@ app.use('/api/project', require('./routes/project'));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     console.log(req);
-    // var err = new Error('Not Found');
+    // let err = new Error('Not Found');
     // err.status = 404;
     // next(err);
     res.redirect('/404.html?from=' + encodeURIComponent(req.originalUrl));
@@ -44,4 +61,5 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+global.memory = {};
 module.exports = app;
