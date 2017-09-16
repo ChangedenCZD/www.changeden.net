d528d7759189b654661fd001e8f39f665ae79a3c
Changeden <1499450463@qq.com>
2017-09-16 17:05:49 +0800

    优化代码

--- utils/server/DbUtils.js
+++ utils/server/DbUtils.js
@@ -12,8 +12,19 @@ const client = (() => {
         host: config.official_web_site.host.remote,
         user: config.official_web_site.admin.account,
         password: config.official_web_site.admin.password,
-        database: config.official_web_site.name
+        database: config.official_web_site.name,
+        multipleStatements: true
     });
 })();
+const DAEMON_SQL = `show tables;`;
+
+function daemon () {
+    console.log(new Date());
+    client.query(DAEMON_SQL, [], () => {
+        setTimeout(daemon, 44444);
+    });
+}
+
+daemon();
 module.exports = mysql;
 module.exports.client = client;
