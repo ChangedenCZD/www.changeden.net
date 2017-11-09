9a4ca5019fc1f45f90f30686a7e44a48684ef0a9
Changeden <1499450463@qq.com>
2017-11-09 12:16:26 +0800

    加入自动部署

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let bodyParser = require('body-parser');
 let lessMiddleware = require('less-middleware');
 let app = express();
 // view engine setup
+let dirname = __dirname;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
@@ -20,12 +21,21 @@ app.all('*', function (req, res, next) {
     next();
 });
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+    extended: false,
+    keepExtensions: true,
+    parameterLimit: 1000000,
+    limit: '50mb',
+    uploadDir: dirname + '/upload'
+}));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
+app.use('/update', require('./routes/update'));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     console.log(req);
